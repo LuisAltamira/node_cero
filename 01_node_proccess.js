6d6101abebd9f6c9f609a3197709a7c8747f0160
LuisAltamira <luisaltamira@newemage.com>
2016-10-11 17:31:09 -0600

    clase 2 terminada

--- 01_node_proccess.js
+++ 01_node_proccess.js
@@ -2,6 +2,7 @@
 
 function singleThread() {
     process.argv[2] = 'algo aqui';
+
     console.log(process.argv[2]);
 }
 
---
+++ 03_hola_mundo_assets.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const http = require('http').createServer(webServer),
+    form = require('fs').readFileSync('./assets/form.html'),
+    querystring = require('querystring'),
+    util = require('util');
+
+let dataString = '';
+
+function webServer(req, res){
+    if (req.method == 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(form);
+    }
+    if (req.method == 'POST') {
+        req
+            .on('data', data => dataString += data)
+            .on('end', () => {
+                let dataObject = querystring.parse(dataString),
+                    dataJSON = util.inspect(dataObject),
+                    html = `
+                        <p>Los datos enviados por POST</p>
+                        <p>${dataJSON}</p>
+                    `;
+                console.log(html);
+                res.end(html);
+            });
+    }
+}
+
+http.listen(3000,'localhost', () => console.log('Servidor corriendo en http://localhost:3000'));
